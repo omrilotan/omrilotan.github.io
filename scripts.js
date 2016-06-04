e4e719e34f71ad58f860b0ac87e2b8405e2c2cd5
Omri Lotan <omri@hamadgera.com>
2016-06-04 14:33:48 +0300

    updates

--- scripts.js
+++ scripts.js
@@ -11,15 +11,11 @@
 (function () {
     if (!window.console || !console.log) { return; }
 
-    console.log("%cHey there, good to see you.\n\n%cFeel free to contact me at %comri@hamadgera.com%c\n\n\n",
-        "font:normal 14px/18px monospace; color:#900;",
-        "",
-        "font:normal 12px/16px monospace; color:#ccc; background:#444; padding:.25em .5em; border-radius:3px;",
+    console.log("\n%cHey there, good to see you.\n%c\n",
+        "font:normal 12px/16px monospace; color:#ccc; background:#444; padding:.5em 1em; border-radius:3px;",
         "");
 }());
 
-(function (collection) {
-    [].forEach.call(collection, function (item) {
-        item.addEventListener('focus', item.select);
-    });
-}(document.querySelectorAll("input[autohighlight], textarea[autohighlight]")));
+document.body.addEventListener("focus", function autohighlight (evt) {
+    evt.target.getAttribute("autohighlight") !== null && evt.target.select();
+}, true);
