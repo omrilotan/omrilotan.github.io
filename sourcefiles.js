bd3d5a18d6fc1093dfdab42e3e667cca6725a1d7
Omri <omri@hamadgera.com>
2017-06-08 16:12:39 +0300

    nothing really

--- sourcefiles.js
+++ sourcefiles.js
@@ -3,15 +3,6 @@
             document.styleSheets.length > 0 ?
             document.styleSheets[0] :
             document.createElement("style");
-        addCssRule = function loadr$_addRule (selector, rules) {
-            if (typeof sheet.insertRule === "function") {
-                sheet.insertRule(selector + "{" + rules + "}", 1);
-            } else if (typeof sheet.addRule === "function") {
-                sheet.addRule(selector, rules, 1);
-            } else {
-                sheet.appendChild(document.createTextNode(selector + "{" + rules + "}"));
-            }
-        };
 
     addCssRule("a.source[href*=\"github\"],a.source[href*=\"bitbucket\"]",
     "   display : block;  \
@@ -21,16 +12,8 @@
         right   : -68px;  \
         padding : 0 50px; \
         margin  : 0;      \
-        -webkit-transform       : rotate(45deg); \
-           -moz-transform       : rotate(45deg); \
-            -ms-transform       : rotate(45deg); \
-             -o-transform       : rotate(45deg); \
-                transform       : rotate(45deg); \
-        -webkit-transform-origin: top left;      \
-           -moz-transform-origin: top left;      \
-            -ms-transform-origin: top left;      \
-             -o-transform-origin: top left;      \
-                transform-origin: top left;      \
+        transform       : rotate(45deg); \
+        transform-origin: top left;      \
         font           : normal 12px/24px monospace;   \
         background     : #4078c0;                      \
         color          : #fff;                         \
@@ -39,4 +22,15 @@
         text-shadow    : none;                         \
     ");
     addCssRule("a.source[href*=\"github\"]:before,a.source[href*=\"bitbucket\"]:before", "content: \"source\";");
+
+    function addCssRule (selector, rules) {
+        if (typeof sheet.insertRule === "function") {
+            sheet.insertRule(selector + "{" + rules + "}", 1);
+        } else if (typeof sheet.addRule === "function") {
+            sheet.addRule(selector, rules, 1);
+        } else {
+            sheet.appendChild(document.createTextNode(selector + "{" + rules + "}"));
+        }
+    };
+
 }(document));
