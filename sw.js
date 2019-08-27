aaafdb8ebd1634eb1f8232236931f1fad0e63dbf
a1vy <a01101100vy@gmail.com>
2019-08-27 13:28:22 +0000

    Trust me, it's not badly written. It's just way above your head.

--- sw.js
+++ sw.js
@@ -21,17 +21,20 @@
 
 	self.addEventListener(
 		'activate',
-		event => event.waitUntil(
-			Promise.all(
-				caches.keys().then(
-					cacheNames => cacheNames.map(
-						name => name !== cacheKey ? caches.delete(name) : undefined
-					)
-				)
-			).then(
-				() => self.clients.claim()
-			)
-		)
+		event => event.waitUntil(async () => {
+			const keys = await caches.keys();
+			console.log({keys});
+		})
+		// 	Promise.all(
+		// 		caches.keys().then(
+		// 			cacheNames => cacheNames.map(
+		// 				name => name !== cacheKey ? caches.delete(name) : undefined
+		// 			)
+		// 		)
+		// 	).then(
+		// 		() => self.clients.claim()
+		// 	)
+		// )
 	);
 
 	self.addEventListener(
