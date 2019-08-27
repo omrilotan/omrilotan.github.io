2b741c82009de94420bc41b92073a565a05de2dc
a1vy <a01101100vy@gmail.com>
2019-08-27 12:44:40 +0000

    It's 2016; why are we using ColdFusion?!

--- sw.js
+++ sw.js
@@ -1,13 +1,46 @@
-self.addEventListener(
-	'install',
-	event => event.waitUntil(
-		caches.open('sw-cache').then(cache => cache.add('index.html'))
-	)
-);
+const meta = (name, tag = document.querySelector(`meta[name="${name}"]`)) => tag && tag.getAttribute('content');
+const CACHED_FILES = [
+	'index.html',
+];
 
-self.addEventListener(
-	'fetch',
-	event => event.respondWith(
-		caches.match(event.request).then(response => response || fetch(event.request))
-	)
-);
+(() => {
+	const cacheKey = meta('sw-cache-key');
+	if (!cacheKey) {
+		return;
+	}
+
+	self.addEventListener(
+		'install',
+		event => event.waitUntil(
+			caches.open(cacheKey).then(
+				cache => cache.addAll(CACHED_FILES)
+			).then(
+				() => self.skipWaiting()
+			)
+		)
+	);
+
+	self.addEventListener(
+		'activate',
+		event => event.waitUntil(
+			Promise.all(
+				caches.keys().then(
+					cacheNames => cacheNames.map(
+						name => name !== cacheKey && caches.delete(name)
+					)
+				)
+			).then(
+				() => self.clients.claim()
+			)
+		)
+	);
+
+	self.addEventListener(
+		'fetch',
+		event => event.respondWith(
+			caches.match(event.request).then(
+				response => response || fetch(event.request)
+			)
+		)
+	);
+});
