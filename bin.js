eb839fcefd85fe97a881861a82c947689ef4e7ec
Omri Lotan <omrilotan@users.noreply.github.com>
2019-09-18 16:05:14 +0300

    move to dev.to - cli

--- bin.js
+++ bin.js
@@ -19,7 +19,7 @@ console.log(
 	[
 		'Hi.\nMy name is Omri.'.bold,
 		`Most of my projects and projects I contribute to are hosted on ${link('GitHub', 'https://github.com/omrilotan')} and are available as open-source.`,
-		`I write articles about coding on ${link('Medium', 'https://medium.com/@omrilotan')}. And maintain ${link('a small website', 'https://omrilotan.com')} with more information.`,
+		`I write articles about coding on ${link('Dev.to', 'https://dev.to/omrilotan')}. And maintain ${link('a small website', 'https://omrilotan.com')} with more information.`,
 	].join('\n')
 );
 
