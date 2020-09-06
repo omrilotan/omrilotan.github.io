c88fbbb42e86deff7b030558f388711750b23dd8
a1vy <a01101100vy@gmail.com>
2020-09-06 07:53:12 +0000

    Does not work.

--- sw.js
+++ sw.js
@@ -1,42 +1,4 @@
-(() => {
-	const version = new URL(location).searchParams.get('ck');
-
-	if (!version) { return; }
-
-	const cacheKey = `cache-key-${version}`;
-
-	const CACHED_FILES = [
-		'index.html',
-	];
-
-	self.addEventListener(
-		'install',
-		event => event.waitUntil(
-			caches.open(cacheKey).then(
-				cache => cache.addAll(CACHED_FILES)
-			)
-		)
-	);
-
-	self.addEventListener(
-		'activate',
-		event => event.waitUntil(
-			caches.keys().then(
-				keys => keys.filter(
-					key => key !== cacheKey
-				).forEach(
-					key => caches.delete(key)
-				)
-			)
-		)
-	);
-
-	self.addEventListener(
-		'fetch',
-		event => event.respondWith(
-			caches.match(event.request).then(
-				response => response || fetch(event.request)
-			)
-		)
-	);
-})();
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"NqYy":[function(require,module,exports) {
+!function(){var e=new URL(location).searchParams.get("ck");if(e){var t="cache-key-".concat(e),n=["index.html"];self.addEventListener("install",function(e){return e.waitUntil(caches.open(t).then(function(e){return e.addAll(n)}))}),self.addEventListener("activate",function(e){return e.waitUntil(caches.keys().then(function(e){return e.filter(function(e){return e!==t}).forEach(function(e){return caches.delete(e)})}))}),self.addEventListener("fetch",function(e){return e.respondWith(caches.match(e.request).then(function(t){return t||fetch(e.request)}))})}}();
+},{}]},{},["NqYy"], null)
+//# sourceMappingURL=sw.js.map
