f2aca719bd4ae047cbd51091b0c96d05fbcc3191
a1vy <a01101100vy@gmail.com>
2023-02-20 23:28:51 +0000

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

--- sw.js
+++ sw.js
@@ -1,42 +1,37 @@
 (() => {
-	const version = new URL(location).searchParams.get('ck');
+	const version = new URL(location).searchParams.get("ck");
 
-	if (!version) { return; }
+	if (!version) {
+		return;
+	}
 
 	const cacheKey = `cache-key-${version}`;
 
-	const CACHED_FILES = [
-		'index.html',
-	];
+	const CACHED_FILES = ["index.html"];
 
-	self.addEventListener(
-		'install',
-		event => event.waitUntil(
-			caches.open(cacheKey).then(
-				cache => cache.addAll(CACHED_FILES)
-			)
+	self.addEventListener("install", (event) =>
+		event.waitUntil(
+			caches.open(cacheKey).then((cache) => cache.addAll(CACHED_FILES))
 		)
 	);
 
-	self.addEventListener(
-		'activate',
-		event => event.waitUntil(
-			caches.keys().then(
-				keys => keys.filter(
-					key => key !== cacheKey
-				).forEach(
-					key => caches.delete(key)
+	self.addEventListener("activate", (event) =>
+		event.waitUntil(
+			caches
+				.keys()
+				.then((keys) =>
+					keys
+						.filter((key) => key !== cacheKey)
+						.forEach((key) => caches.delete(key))
 				)
-			)
 		)
 	);
 
-	self.addEventListener(
-		'fetch',
-		event => event.respondWith(
-			caches.match(event.request).then(
-				response => response || fetch(event.request)
-			)
+	self.addEventListener("fetch", (event) =>
+		event.respondWith(
+			caches
+				.match(event.request)
+				.then((response) => response || fetch(event.request))
 		)
 	);
 })();
